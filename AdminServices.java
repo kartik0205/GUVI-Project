ceb6bbeafc9f95607a4376c4f212d65573bc9ecb
Kartik Shrivastava <kartikshrivastava0205@gmail.com>
2024-11-10 21:13:57 +0530

    Create AdminServices.java
    Contains functionalities for Admin.

---
+++ AdminServices.java
@@ -0,0 +1,17 @@
+package com.petadoption.services;
+
+import com.petadoption.models.User;
+
+public class AdminService {
+    public void manageUser (User user) {
+        // Logic to create/update/delete user
+    }
+
+    public void managePetListings() {
+        // Logic to approve/reject pet listings
+    }
+
+    public void manageSystemSettings() {
+        // Logic to manage system settings
+    }
+}
