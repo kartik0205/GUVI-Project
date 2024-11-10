4ca0e947004c0d3e9e0c835cc93013c97332313c
Kartik Shrivastava <kartikshrivastava0205@gmail.com>
2024-11-10 21:14:27 +0530

    Create ShelterService.java

---
+++ ShelterService.java
@@ -0,0 +1,17 @@
+package com.petadoption.services;
+
+import com.petadoption.models.Pet;
+
+public class ShelterService {
+    public void listPet(Pet pet) {
+        // Logic to list a pet
+    }
+
+    public void interactWithAdopters() {
+        // Logic to communicate with potential adopters
+    }
+
+    public void trackAdoptionApplications() {
+        // Logic to track applications
+    }
+}
