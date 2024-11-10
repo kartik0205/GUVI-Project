18de857c54879153ce2083c910ae22f5f8a1c280
Kartik Shrivastava <kartikshrivastava0205@gmail.com>
2024-11-10 21:16:24 +0530

    Create AdopterService.java
     Contains functionalities for Adopters.

---
+++ AdopterService.java
@@ -0,0 +1,18 @@
+package com.petadoption.services;
+
+import com.petadoption.models.AdoptionApplication;
+import com.petadoption.models.Pet;
+
+public class AdopterService {
+    public void browsePets() {
+        // Logic to browse available pets
+    }
+
+    public void submitApplication(AdoptionApplication application) {
+        // Logic to submit adoption application
+    }
+
+    public void interactWithShelters() {
+        // Logic to communicate with shelters
+    }
+}
