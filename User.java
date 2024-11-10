7192cf81405e362cb53376a4d940d1cff777155a
Kartik Shrivastava <kartikshrivastava0205@gmail.com>
2024-11-10 21:11:40 +0530

    Create User.java
     Represents a user in the system.

---
+++ User.java
@@ -0,0 +1,9 @@
+package com.petadoption.models;
+
+public class User {
+    private String name;
+    private String email;
+    private String role; // Admin, Shelter, Adopter
+
+    // Constructors, Getters, and Setters
+}
